195193ddd961024a260fc7ef400f4190052260c0
0110341 <124908461+0110341@users.noreply.github.com>
2023-12-06 10:35:08 -0500

    basic encryption and decryption

--- Encryption.py
+++ Encryption.py
@@ -1,14 +1,53 @@
+# def encrypt(message, shift):
+#     # To get the unicode of a character we use the function ord("a")
+#     # To get the character of a unicode we use the function chr(97)
+#     shift = shift % 26
+#
+#     encrypted_message = ""
+#     for character in message:
+#         unicode = ord(character)
+#         shifted_unicode = unicode + shift
+#         encrypted_character = chr(shifted_unicode)
+#         encrypted_message += encrypted_character
+#
+#     return encrypted_message
+#
+#
+# def decrypt(message, shift):
+#     shift = shift % 26
+#     decrypted_message = ""
+#
+#     for character in message:
+#         unicode = ord(character)
+#         shifted_unicode = unicode - shift
+#         decrypted_character = chr(shifted_unicode)
+#         decrypted_message += decrypted_character
+#
+#     return decrypted_message
+#
+#
+# if __name__ == '__main__':
+#     message = input("Enter a message to encrypt: ")
+#     shift = int(input("Enter shift value: "))
+#     encrypted_message = encrypt(message, shift)
+#     decrypted_message = decrypt(encrypted_message, shift)
+#     print("Original Message:", message)
+#     print("Encrypted Message:", encrypted_message)
+#     print("Decrypted Message:", decrypted_message)
+
 def encrypt(message, shift):
-    # To get the unicode of a character we use the function ord("a")
-    # To get the character of a unicode we use the function chr(97)
     shift = shift % 26
 
     encrypted_message = ""
     for character in message:
-        unicode = ord(character)
-        shifted_unicode = unicode + shift
-        encrypted_character = chr(shifted_unicode)
-        encrypted_message += encrypted_character
+        if character.isalpha():  # Check if the character is a letter
+            unicode = ord(character)
+            shifted_unicode = (unicode - ord('A' if character.isupper() else 'a') + shift) % 26 + ord('A' if character.isupper() else 'a')
+            encrypted_character = chr(shifted_unicode)
+            encrypted_message += encrypted_character
+        else:
+            # Keep non-letter characters unchanged
+            encrypted_message += character
 
     return encrypted_message
 
@@ -18,10 +57,14 @@ def decrypt(message, shift):
     decrypted_message = ""
 
     for character in message:
-        unicode = ord(character)
-        shifted_unicode = unicode - shift
-        decrypted_character = chr(shifted_unicode)
-        decrypted_message += decrypted_character
+        if character.isalpha():  # Check if the character is a letter
+            unicode = ord(character)
+            shifted_unicode = (unicode - ord('A' if character.isupper() else 'a') - shift) % 26 + ord('A' if character.isupper() else 'a')
+            decrypted_character = chr(shifted_unicode)
+            decrypted_message += decrypted_character
+        else:
+            # Keep non-letter characters unchanged
+            decrypted_message += character
 
     return decrypted_message
 
