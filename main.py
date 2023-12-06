679c9a483f5b67230fd724c3ab1f2d69afd4ff2f
Adriano <adriano.c.e@gmail.com>
2023-12-06 12:19:03 -0500

    Input from user - ok

--- main.py
+++ main.py
@@ -3,21 +3,24 @@ def decrypt_caesar_cipher(ciphertext, shift):
 
     for char in ciphertext:
         if char.isalpha():
-            
             is_upper = char.isupper()
-            
-            decrypted_char = chr((ord(char) - shift - ord('A' if is_upper else 'a')) % 26 + ord('A' if is_upper else 'a'))
+            decrypted_char = chr((ord(char) + shift - ord('A' if is_upper else 'a')) % 26 + ord('A' if is_upper else 'a'))
             decrypted_message += decrypted_char
         else:
-            
             decrypted_message += char
 
     return decrypted_message
 
-# Example usage
-encrypted_message = "Otzkr iutloxsy znk gxxobgr ul znk Kgmrk gz jgct. Ykiaxk znk vgiqgmk gz znk jxuv futk sgxqkj cozn g hrak lrgm. Ktixevz grr yahykwaktz xkvuxzy."
-shift_amount = 6
+# Get user input for the message
+encrypted_message = input("Type the message: ")
+
+# Get user input for the key
+shift_amount = int(input("Type the key (a number): "))
+
+# Decrypt the message
 decrypted_message = decrypt_caesar_cipher(encrypted_message, shift_amount)
 
+# Print the results
 print("Encrypted message:", encrypted_message)
 print("Decrypted message:", decrypted_message)
+
